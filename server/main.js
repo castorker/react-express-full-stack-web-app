6417d897ebfc7e1e01f91ad8293237695fe8b45b
Eduardo <eduardo.cxcorreio@gmail.com>
2017-11-24 12:23:53 +0100

    Bundling the application with Browserify.

--- server/main.js
+++ server/main.js
@@ -5,5 +5,5 @@ var app = express();
 app.get('/', function (req, res) {
         res.render('./../app/index.ejs', {});
     })
-    .use(express.static(__dirname + '/../app'))
+    .use(express.static(__dirname + '/../.tmp'))
     .listen(3000);
