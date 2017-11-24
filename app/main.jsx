889dfbb4358d8266cd59b7a41003ae83c8152290
Eduardo <eduardo.cxcorreio@gmail.com>
2017-11-24 15:47:57 +0100

    Created React Component - QuibbleEntityList.

--- app/main.jsx
+++ app/main.jsx
@@ -1,1 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
 console.log("Hello, transpiling JSX!");
+
+var QuibbleEntityList = require('./components/QuibbleEntityList.jsx');
+
+ReactDOM.render(<QuibbleEntityList/>, app);
